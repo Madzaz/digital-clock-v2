1b26e75d0334b8c23a8d49142992bf5202640225
Mladen <141567296+Madzaz@users.noreply.github.com>
2024-04-26 17:54:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function timeNow() {
+  const now = new Date();
+
+  let hours = now.getHours().toString().padStart(2, `0`);
+  let minutes = now.getMinutes().toString().padStart(2, `0`);
+  let seconds = now.getSeconds().toString().padStart(2, `0`);
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  document.getElementById("clock").textContent = timeString;
+}
+
+setInterval(timeNow, 100);
